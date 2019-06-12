08698e5eb456ba1d83715df91230f6edbbb25f81
lucasGabrielDeAA <lg.technus@gmail.com>
2019-06-12 17:45:49 -0300

    Provider added

--- src/App.js
+++ src/App.js
@@ -1,101 +1,19 @@
-import React, { useState, useEffect, useReducer } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { Provider } from 'react-redux';
+
 import './App.css';
 
-const initialState = {
-  count: 0,
-  click: 0,
-};
+import store from './store';
 
-function reducer(state = initialState, action) {
-  switch (action.type) {
-    case 'INCREASE_COUNT_REQUEST':
-      return {
-        ...state,
-        count: state.count + 1,
-        click: state.click + 1,
-      };
-    case 'DECREASE_COUNT_REQUEST':
-      return {
-        ...state,
-        count: state.count - 1,
-        click: state.click + 1,
-      };
-    case 'RESET_REQUEST':
-      return {
-        ...state,
-        count: 0,
-        click: 0,
-      };
-    default:
-      return state;
-  }
-}
+import Container from './components/Container';
 
-function App(props) {
-  const [todos, setTodos] = useState([]);
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  useEffect(() => {
-    document.title = `${props.name !== '' ? props.name : 'User'}, you have ${todos.length} todos`;
-  }, [todos]);
-
-  const addTodo = () => {
-    setTodos([ ...todos, { id: Math.random(), text: '' } ]);
-  }
-
-  const onChangeTodo = (name, e, id) => {
-    const { value } = e.target;
-    setTodos(todos.map(todo => ({ ...todo, [name]: todo.id === id ? value : todo.value })));
-  }
-
-  const handleRemoveTodo = id => setTodos(todos.filter(todo => todo.id !== id));
-
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-
-      <div className="container">
-        <div className="buttonsContainer">
-          <button className="confirmButton" onClick={addTodo}>
-            <p> + todo </p>
-          </button>
-        </div>
-
-        {todos.map((todo, index) => (
-          <div key={String(todo.id)} className="todo">
-            <input
-              type="text"
-              placeholder={`Todo ${index + 1}`}
-              value={todo.text}
-              onChange={value => onChangeTodo('text', value, todo.id)}
-            />
-
-            <button className="dangerButton" onClick={() => handleRemoveTodo(todo.id)}>
-              <p> - todo </p>
-            </button>
-          </div>
-        ))}
-
-        <div className="buttonsContainer">
-          <button className="confirmButton" onClick={() => dispatch({ type: 'INCREASE_COUNT_REQUEST' })}>
-            <p> + count </p>
-          </button>
-
-          <button className="confirmButton" onClick={() => dispatch({ type: 'DECREASE_COUNT_REQUEST' })}>
-            <p> - count </p>
-          </button>
-
-          <button className="confirmButton" onClick={() => dispatch({ type: 'RESET_REQUEST' })}>
-            <p> reset </p>
-          </button>
-        </div>
-
-        <p className="bright">{`To reach the value ${state.count}, you clicked ${state.click} times`}</p>
+    <Provider store={store}>
+      <div className="App">
+        <Container />
       </div>
-    </div>
+    </Provider>
   );
 }
 
